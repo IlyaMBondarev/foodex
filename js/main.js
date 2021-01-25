212d1312e0eb54de693959b2bb134a50618e27fd
IlyaMBondarev <60775995+IlyaMBondarev@users.noreply.github.com>
2021-01-25 16:29:18 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+document.querySelector('.page').classList.add('loaded');
+
+
+//карусель-слайдер на странице услуги
+
+$(document).ready(function() {
+    if ($('.capabilities').length) {
+        let owl = $('.capabilities .owl-carousel');
+        if (owl.length) {
+            owl.owlCarousel({
+                items: 1,
+                margin: 25,
+                mouseDrag: false,
+                nav: false,
+                dots: false,
+            });
+
+            $('.capabilities__arrow_next').click(function () {
+                owl.trigger('next.owl.carousel');
+            });
+
+            $('.capabilities__arrow_prev').click(function () {
+                owl.trigger('prev.owl.carousel', [300]);
+            });
+        }
+    }
+});
